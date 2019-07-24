3effd0e0321cc4ee8f54fa68571e37826815bdb4
Eliza Irwin <elizairwin@gmail.com>
2019-07-24 12:25:41 -0400

    need to do javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,27 @@
-var savedMovieList = ["Toy Story", "Toy Story 2", "Toy Story 3", "Toy Story 4"];
-    var movieList = [];
+var giphyList = [];
 
     function createCard(response) {
-      // Create a new boostrap card container
       var article = $("<article>");
       article.addClass("card");
 
-      // Create an image elment, add attributes, and append to figure
-      var posterImage = $("<img>");
-      posterImage.attr("src", response.Poster);
-      posterImage.attr("alt", response.Title + " Poster");
+      var giphyImage = $("<img>");
+      giphyImage.attr("src", response.data[17].original_still);
 
-      var moviePoster = $("<figure>");
-      moviePoster.append(posterImage);
-      article.append(moviePoster);
+      var giphyImage = $("<figure>");
+      giphyImage.append(giphyImage);
+      article.append(giphyImage);
 
-      // Create a new card body container
       var cardBody = $("<div>");
       cardBody.addClass("card-body");
-
-      // Add movie name
-      var movieName = $("<h1>");
-      movieName.addClass("card-title");
-      movieName.html(response.Title);
-      article.append(movieName);
-
-      // Add description
-      var movieDescription = $("<p>");
-      movieDescription.addClass("card-text");
-      movieDescription.html(response.Plot);
-      article.append(movieDescription);
-
-      // Append the new card to the HTML body
+      
       $("#gif-section").append(article);
 
     }
 
-    function getMovie(movie) {
+    function getGif(giphy) {
 
-      // queryURL endpoint for OMDB API
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=UWJ78tsEAy2ys9CkuDRhr6Wwh5XWMynL&limit=5";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=UWJ78tsEAy2ys9CkuDRhr6Wwh5XWMynL&limit=5";
 
-
-      // AJAX call to OBMD API with promise and callback handler
       $.ajax({
         url: queryURL,
         method: "GET"
@@ -50,34 +29,40 @@ var savedMovieList = ["Toy Story", "Toy Story 2", "Toy Story 3", "Toy Story 4"];
         if (response.Response === "False") {
           alert(response.Error);
         }
-        else if (movieList.indexOf(response.Title) >= 0) {
-          alert ("Movie already in List!")
+        else if (giphy.indexOf(response.data[17].original_still) >= 0) {
+          alert ("Gif already in List!")
         }
         else {
           createCard(response);
-          movieList.push(response.Title);
+          gif.push(response.Title);
         }
       });
 
     }
 
-    // Create a bootstrap card for each item in the movieList array
+    for (var i = 0; i < giphyList.length; i++) {
 
-    for (var i = 0; i < movieList.length; i++) {
-
-      var movie = movieList[i];
-      getMovie(movie);
+      var giphy = giphy[i];
+      getGif(giphy);
 
     }
 
-    //
-    // TO DO: Create button click handler to get the form submission,
-    // and call the AJAX function, passing the name of the new movie
-    //
-
     $("#search").click(function() {
 
-      var movie = $("#film-name").val();
-      getMovie(movie);
+      var giphy = $("#gif-name").val();
+      getGif(giphy);
       
     });
+
+    function animateGif(){
+        console.log("click")
+        var state = $(this).attr("data-state");
+        if(state === "still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    
+    }
